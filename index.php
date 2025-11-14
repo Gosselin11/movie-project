e79d76f29d128e432758cb0b94d766a69a64128a
mithridatem <mathieumithridate@adrar-formation.com>
2025-11-14 14:41:09 +0100

    feat: update router

--- index.php
+++ index.php
@@ -6,4 +6,20 @@ $url = parse_url($_SERVER["REQUEST_URI"]);
 
 $path = isset($url["path"]) ? $url["path"] : "/";
 
-dump($path);
+switch ($path) {
+    case '/':
+        echo "Home";
+        break;
+    case '/login':
+        echo "login";
+        break;
+    case '/logout':
+        echo "deconnexion";
+        break;
+    case '/register':
+        echo "inscription";
+        break;
+    default:
+        echo "error 404";
+        break;
+}
