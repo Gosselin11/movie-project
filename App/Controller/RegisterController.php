38cfa1e630ef0aa55ab9e50e9344aa51fe7ab20d
Kevin <kevin.gosselin29@gmail.com>
2025-11-18 16:36:40 +0100

    Exercice Création de compte projet Movie

--- App/Controller/RegisterController.php
+++ App/Controller/RegisterController.php
@@ -9,12 +9,12 @@ use App\Utils\Tools;
 class RegisterController
 {
     private Account $accountModel;
-    
+
     public function __construct()
     {
         $this->accountModel = new Account();
     }
-    
+
     /**
      * Méthode pour rendre une vue avec un template
      * @param string $template Le nom du template à inclure
@@ -30,22 +30,65 @@ class RegisterController
     public function addAccount()
     {
         //Verifier si le formulaire est submit
-        //vérifier si les champs sont remplis
-            //Si ok on continu
-            //Sinon on affiche un message d'erreur
-        //vérifier si les 2 password sont identiques
-            //Si identique on continu
-            //Si différent on affiche un message d'erreur
-        //vérifier si le compte n'existe pas
-        //si il n'existe pas -> 
-            //créer un objet Account
-            //setter les valeurs
-            //setter grant_id = 1
-            //hasher le password password_hash(mot de passe en clair, PASSWORD_DEFAULT)
-            //Afficher une message qui indique que le compte à été ajouté en BDD
-        //Si il existe 
-            //Afficher un message d'erreur qui indique que le compte existe déja
-        
+        if (isset($_POST["submit"])) {
+            $firstname = $_POST["firstname"] ?? "";
+            $lastname = $_POST["lastname"] ?? "";
+            $email = $_POST["email"] ?? "";
+            $password = $_POST["password"] ?? "";
+            $confirmPassword = $_POST["confirm-password"] ?? "";
+
+            //vérifier si les champs sont remplis
+            if (!empty($firstname) && !empty($lastname) && !empty($email) && !empty($password) && !empty($confirmPassword)) {
+
+                //Si ok on continu  
+                //Sinon on affiche un message d'erreur
+                //vérifier si les 2 password sont identiques
+                if ($password === $confirmPassword) {
+                    //Si identique on continu
+                    //Si différent on affiche un message d'erreur  
+                    if ($password !== $confirmPassword) {
+                        $data["error"] = "Les deux mots de passe ne sont pas identiques.";
+                    }
+                    //vérifier si le compte n'existe pas
+                    if (!$this->accountModel->isAccountExistsByEmail($email)) {
+                        //si il n'existe pas -> 
+                        $grant = new Grant(null);
+                        $grant->setId(1);
+                        //créer un objet Account
+                        $account = new Account();
+                        //setter les valeurs
+                        $account->setFirstname($firstname);
+                        $account->setLastname($lastname);
+                        $account->setEmail($email);
+                        //setter grant_id = 1
+                        $account->setGrant($grant);
+                        //hasher le password password_hash(mot de passe en clair, PASSWORD_DEFAULT)
+                        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+                        $account->setPassword($hashedPassword);
+                        //Afficher une message qui indique que le compte à été ajouté en BDD
+                        $this->accountModel->saveAccount($account);
+                        $data["valid"] = "Le compte a été ajouté en BDD.";
+                        //Si il existe 
+                        //Afficher un message d'erreur qui indique que le compte existe déja
+
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         return $this->render("register_account", "Inscription");
     }
 }
